6822a415784b01fcc0035af192597916c31cdd53
kinisoftware <kinisoftware@gmail.com>
2017-03-08 23:00:30 +0100

    Walkthrough videos done

--- app/src/main/java/com/kinisoftware/comingsoon/MovieActivity.java
+++ app/src/main/java/com/kinisoftware/comingsoon/MovieActivity.java
@@ -2,7 +2,9 @@ package com.kinisoftware.comingsoon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
+import com.kinisoftware.comingsoon.adapter.MovieArrayAdapter;
 import com.kinisoftware.comingsoon.model.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -11,6 +13,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
@@ -18,11 +21,18 @@ import cz.msebera.android.httpclient.Header;
 public class MovieActivity extends AppCompatActivity {
 
     List<Movie> movies;
+    MovieArrayAdapter movieAdapter;
+    ListView lvMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        lvMovies = (ListView) findViewById(R.id.lvMovies);
+        movies = new ArrayList<>();
+        movieAdapter = new MovieArrayAdapter(this, movies);
+        lvMovies.setAdapter(movieAdapter);
 
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
@@ -31,7 +41,8 @@ public class MovieActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
                     JSONArray jsonArrayMovies = response.getJSONArray("results");
-                    movies = Movie.fromArray(jsonArrayMovies);
+                    movies.addAll(Movie.fromArray(jsonArrayMovies));
+                    movieAdapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
---
+++ app/src/main/java/com/kinisoftware/comingsoon/adapter/MovieArrayAdapter.java
@@ -0,0 +1,48 @@
+package com.kinisoftware.comingsoon.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kinisoftware.comingsoon.R;
+import com.kinisoftware.comingsoon.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(@NonNull Context context, @NonNull List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        ImageView ivMovieImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        ivMovieImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivMovieImage);
+
+        return convertView;
+    }
+}
